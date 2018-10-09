c7ea588fba988863e4381c6c1eb93b972ca8cf1b
James Bear <jamesgrizzly@gmail.com>
2018-10-09 17:08:09 +0800

    Modify test model path

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ content = f.read()
 f.close()
 from inference_helper import InferenceHelper
 infer_helper = InferenceHelper()
-infer_helper.init(model_path='models/NER_cn_names_artificial_data_0.9009_0.9036_1.h5', mode='name', timeit=False)
+infer_helper.init(model_path='models/NER_cn_names_artificial_data_0.8850_0.8850_23.h5', mode='name', timeit=False)
