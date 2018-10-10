01a6187f39ee796c2f7e181e2f0bea4ddf313d04
James Bear <jamesgrizzly@gmail.com>
2018-10-10 14:29:31 +0800

    Make the test inference script more user friendly
    Usage:
    ipython
    run test.py
    infer(content)

--- test.py
+++ test.py
@@ -4,7 +4,31 @@ content = f.read()
 f.close()
 from inference_helper import InferenceHelper
 infer_helper = InferenceHelper()
-#infer_helper.init(model_path='models/NER_cn_names_artificial_data_0.9830_0.9821_14.h5', mode='name', timeit=False, mute=False)
-infer_helper.init(model_path='models/NER_cn_names_artificial_data_0.9632_0.9640_10.h5', mode='name', timeit=False, mute=False)
+infer_helper.init(model_path='models/NER_cn_names_artificial_data_0.9830_0.9821_14.h5', mode='name', timeit=False, mute=True)
+#infer_helper.init(model_path='models/NER_cn_names_artificial_data_0.9740_0.9727_3.h5', mode='name', timeit=False, mute=True)
+
 def infer(s):
-    _ = infer_helper.infer(s)
+    sentences, tags = infer_helper.infer(s)
+    
+    if len(sentences) > 0 and len(tags.keys()) > 0:
+        tokens = sentences[0]['tokens']
+        bios = tags[list(tags.keys())[0]][0]
+
+        out_str = ''
+        name = ''
+        for i in range(len(tokens)):
+            c = tokens[i]
+            if name == '':
+                if bios[i] == 'B-NAME':
+                    name += c
+                    out_str += '['
+                out_str += c
+            elif bios[i] == 'I-NAME':
+                name += c
+                out_str += c
+            else:
+                out_str += ']' + c
+                name = ''
+        if name != '':
+            out_str += ']'
+        print(out_str)
